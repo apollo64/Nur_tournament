aea028ec7d341721a69ce50d272133bbf0ff010c
apollo64 <apollo64@gmail.com>
2020-11-16 11:21:49 +0600

    Models are set

---
+++ teams/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+
+
+# Create your models here.
+
+class Team(models.Model):
+    name = models.CharField(max_length=120)
+    created = models.DateTimeField(auto_now_add=True)
+    score = models.IntegerField(default=0)
+
+
+class Division(models.Model):
+    name = models.CharField(max_length=10)
+    name_teams = models.ForeignKey(Team,
+                                   on_delete=models.PROTECT,
+                                   related_name='division_name_teams', blank=True)
+
+
+class Game(models.Model):
+    team_1 = models.ForeignKey(Team,
+                               on_delete=models.PROTECT,
+                               related_name='team_1')
+    team_2 = models.ForeignKey(Team,
+                               on_delete=models.PROTECT,
+                               related_name='team_2')
+    result_1 = models.IntegerField(default=0)
+    result_2 = models.IntegerField(default=0)
+    division = models.ForeignKey(Division,
+                                 on_delete=models.PROTECT,
+                                 related_name='division')
+
+
