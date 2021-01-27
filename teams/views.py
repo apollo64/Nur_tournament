adaaa919e37fe4bdfb3edf7a1955bdc461a03bd8
apollo64 <apollo64@gmail.com>
2021-01-27 15:37:16 +0600

    random division in two teams  and display of teams and it is division

--- teams/views.py
+++ teams/views.py
@@ -1,3 +1,5 @@
+import random
+
 from django.shortcuts import render, redirect
 
 # Create your views here.
@@ -29,20 +31,20 @@ from teams.models import Team, Division
 #        else:
 #            return render(request, 'index2.html', context={'form': form})
 #
-class TeamCreateView(FormView):
-    template_name = 'index.html'
-    form_class = TeamForm
-    success_url = 'divisions'
-
-    def form_valid(self, form):
-        # This method is called when valid form data has been POSTed.
-        # It should return an HttpResponse.
-        # form.send_email()
-
-        # return redirect('divisions')
-        for i in form:
-            Team.objects
-        return Response(status=200)
+# class TeamCreateView(FormView):
+#     template_name = 'index.html'
+#     form_class = TeamForm
+#     success_url = 'divisions'
+#
+#     def form_valid(self, form):
+#         # This method is called when valid form data has been POSTed.
+#         # It should return an HttpResponse.
+#         # form.send_email()
+#
+#         # return redirect('divisions')
+#         for i in form:
+#             Team.objects
+#         return Response(status=200)
 
 # class AuthorCreate(CreateView):
 #     model = Team
@@ -56,15 +58,38 @@ def create_teams_normal(request):
         formset = TeamFormset(request.GET or None)
     elif request.method == 'POST':
         formset = TeamFormset(request.POST)
+        list_teams = []
+        team_a = []
+        team_b = []
         if formset.is_valid():
+            try:
+                # a_div = Division.objects.get_or_create(name="A")
+                # b_div = Division.objects.get_or_create(name="A")
+                a_div = Division.objects.get(name="A")
+                b_div = Division.objects.get(name="B")
+            except:
+                Division(name = "A").save()
+                Division(name = "B").save()
+                a_div = Division.objects.get(name="A")
+                b_div = Division.objects.get(name="B")
+            for form in formset:
+                name = form.cleaned_data.get('name')
+                list_teams.append(name)
+                random.shuffle(list_teams)
+                team_a = list_teams[:9]
+                team_b = list_teams[9:]
             for form in formset:
                 # extract name from each form and save
                 name = form.cleaned_data.get('name')
                 # save book instance
                 if name:
-                    Team(name=name).save()
+                    if name in team_a:
+                        Team(name=name, division=a_div).save()
+                    else:
+                        Team(name=name, division=b_div).save()
                     # redirect('divisions')
             # once all books are saved, redirect to book list view
+
             return redirect('divisions')
     return render(request, template_name, {
         'formset': formset,
@@ -72,12 +97,8 @@ def create_teams_normal(request):
     })
 
 
-
 class DivisionTeamView(ListView):
     model = Team
     context_object_name = 'team_division_list'
     template_name = 'divisions.html'
     queryset = Team.objects.all()
-
-
-
